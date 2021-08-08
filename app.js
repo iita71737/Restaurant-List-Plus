d2c673b196fd8049ca9ab6cb21d2ab848b904208
Demilululu <demilululu@gmail.com>
2021-08-09 00:46:58 +0800

    Add facebook login and dotenv

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const methodOverride = require('method-override')
 const session = require('express-session')
 const flash = require('connect-flash')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const usePassport = require('./config/passport')
 require('./config/mongoose')
 const routes = require('./routes')
 
 const app = express()
-const port = 3000
+const PORT = process.env.PORT
 
 const hbs = exphbs.create({ defaultLayout: 'main', extname: '.hbs' })
 hbs.handlebars.registerHelper('ifEqual', function (v1, v2, options) {
@@ -25,25 +29,22 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
-  secret: 'FullOfCats',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
 usePassport(app)
 app.use(flash())
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
   next()
 })
-
-
-
 app.use(routes)
 
 
-app.listen(port, () => {
-  console.log(`Express is now listening to http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Express is now listening to http://localhost:${PORT}`)
 })
--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,9 @@
 const passport = require('passport')
-const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+
+const LocalStrategy = require('passport-local').Strategy
+const FacebookStrategy = require('passport-facebook').Strategy
+
 const User = require('../models/user')
 
 module.exports = (app) => {
@@ -8,20 +12,49 @@ module.exports = (app) => {
   app.use(passport.session())
 
   //Set Local Strategy
-  passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email })
-      .then((user) => {
-        if (!user) {
-          return done(null, false, { message: 'That email is not registered!' })
-        }
-        if (password !== user.password) {
-          return done(null, false, { message: 'Email or Password incorrect!' })
-        }
-        console.log('Login successfully!')
-        return done(null, user)
-      })
-      .catch(error => console.log(error))
-  }))
+  passport.use(new LocalStrategy({ usernameField: 'email' },
+    (email, password, done) => {
+      User.findOne({ email })
+        .then(user => {
+          if (!user) {
+            return done(null, false, { message: 'That email is not registered!' })
+          }
+          bcrypt.compare(password, user.password).then(isMatch => {
+            if (!isMatch) {
+              return done(null, false, { message: 'Email or Password incorrect!' })
+            }
+            return done(null, user)
+          })
+        })
+        .catch(error => done(error, false))
+    }))
+
+  //Set Facebook Strategy
+  passport.use(new FacebookStrategy({
+    clientID: process.env.FACEBOOK_ID,
+    clientSecret: process.env.FACEBOOK_SECRET,
+    callbackURL: process.env.FACEBOOK_CALLBACK,
+    profileFields: ['email', 'displayName']
+  },
+    function (accessToken, refreshToken, profile, done) {
+      const { name, email } = profile._json
+      User.findOne({ email })
+        .then(user => {
+          if (user) {
+            return done(null, user)
+          }
+          const randomPassword = Math.random().toString(36).slice(-8)
+          bcrypt
+            .genSalt(10)
+            .then(salt => bcrypt.hash(randomPassword, salt))
+            .then(hash => {
+              User.create({ name, email, password: hash })
+            })
+            .then(user => done(null, user))
+            .catch(error => done(error, false))
+        })
+    }
+  ));
 
   //Serialise and Deserialise
   passport.serializeUser((user, done) => {
